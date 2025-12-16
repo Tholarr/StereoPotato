a620456f79814bd31dcc6ab7b2a953ed26a49bd9
Tholarr <thomas.cluseau@epitech.eu>
2025-12-16 19:25:11 +0100

    :wrench: Add debug console window to the app

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
 ﻿using Avalonia;
 using System;
+using System.Runtime.InteropServices;
 
 namespace StereoPotato;
 
 sealed class Program
 {
+    // Add a console window to the application so Console.WriteLine outputs are visible
+    [DllImport("kernel32.dll")]
+    private static extern bool AllocConsole();
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        AllocConsole();
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
