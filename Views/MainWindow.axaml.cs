93622d303bb4f7731097dfafcfcf6dadd6342bf2
Tholarr <thomas.cluseau@epitech.eu>
2025-12-16 19:26:39 +0100

    :sparkles: Add dialog file picker

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
 
 namespace StereoPotato.Views;
 
@@ -7,5 +9,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void OpenFile_Click(object? sender, RoutedEventArgs e)
+    {
+        var openFileDialog = new OpenFileDialog
+        {
+            Title = "Open WAV file",
+            AllowMultiple = false,
+            Filters =
+            {
+                new FileDialogFilter
+                {
+                    Name = "WAV audio",
+                    Extensions = { "wav" }
+                }
+            }
+        };
+
+        var result = await openFileDialog.ShowAsync(this);
+
+        if (result != null && result.Length > 0)
+        {
+            Console.WriteLine($"Fichier sélectionné : {result[0]}");
+        }
     }
 }
